99b52f75d269056d290d2e50d3a2a7592a9093f0
Jotunbane <67220764+Jotunbane-lab@users.noreply.github.com>
2022-07-07 11:27:14 +0200

    Changing configuration in favor Entity manager and "getDataSource"

--- src/main/java/lab/jotunbane/FoodStock/configuration/SpringConfig.java
+++ src/main/java/lab/jotunbane/FoodStock/configuration/SpringConfig.java
@@ -1,11 +1,38 @@
 package lab.jotunbane.FoodStock.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"configuration", "services"})
 @EnableJpaRepositories("repositories")
 public class SpringConfig {
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/Foodstock");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("IT123");
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(getDataSource());
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setPackagesToScan("lab.jotunbane.FoodStock.model");
+
+        return entityManager;
+    }
 }
